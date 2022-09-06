8f3507ea7098266fc669f7300a5a7dac71f334d3
zhaoDaCuo <969326265@qq.com>
2022-09-06 16:14:26 +0800

    postcss完成

--- src/main.js
+++ src/main.js
@@ -2,11 +2,18 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+// 引入rem适配
+import 'amfe-flexible'
+// 引入icon 图标
+import '@/assets/fons/iconfont.css'
+// 引入所有vant
+import Vant from 'vant'
+import 'vant/lib/index.css'
+Vue.use(Vant)
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: (h) => h(App)
 }).$mount('#app')
