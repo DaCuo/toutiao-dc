7bd7be1f573e6e808870b3b86cabe3f51033a34a
zhaoDaCuo <969326265@qq.com>
2022-09-06 18:13:29 +0800

    feat:login布局完成

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,6 @@
 <template>
   <div>
-    <span class="iconfont icon-lishi"></span>
-    <van-button type="primary">主要按钮</van-button>
-    <van-button type="info">信息按钮</van-button>
-    <van-button type="default">默认按钮</van-button>
-    <van-button type="warning">警告按钮</van-button>
-    <van-button type="danger">危险按钮</van-button>
-    <div class="box"></div>
+    <router-view></router-view>
   </div>
 </template>
 
@@ -14,10 +8,4 @@
 export default {}
 </script>
 
-<style scoped>
-.box {
-  width: 1.25rem;
-  height: 0.25rem;
-  background-color: red;
-}
-</style>
+<style scoped></style>
